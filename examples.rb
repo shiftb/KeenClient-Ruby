059379b886f1849272b5b891b2195fbb715dc158
dorkitude <kyle@kylewild.com>
2012-03-06 23:23:05 -0600

    add a simple usage example

---
+++ examples.rb
@@ -0,0 +1,28 @@
+require 'keen'
+ 
+# Get these from the keen.io website:
+project_id = 'asdfasldkfjalsdkfalskdfj'
+auth_token = 'asldfjklj325tkl32jaskdlfjaf'
+
+# First you must setup the client:
+keen = Keen::Client.new
+keen.project_id = project_id
+keen.auth_token = auth_token
+
+# Then, you can use that client to send events.  
+
+keen.add_event(:purchases, {
+  :quantity   => @quantity,
+  :user       => @user.hashify,
+  :item       => @item.hashify,
+  :session    => @session.hashify,
+})
+
+keen.add_event(:pageviews, {
+  :user       => @user.hashify,
+  :route      => @current_route,
+  :session    => @session.hashify,
+})
+
+# (These examples pretend your important objects all have a method called
+# "hashify", and that method serializes state information you wish to track.)
