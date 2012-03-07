d65bf8228d3c6a979e7b7780cf2785f3e74688a1
dorkitude <kyle@kylewild.com>
2012-03-07 12:30:33 -0600

    just testing new github keys

--- lib/keen/client.rb
+++ lib/keen/client.rb
@@ -9,22 +9,24 @@ module Keen
       queue = file_handler.rotate_and_process
       queue.each do |job|
         project_id = job.project_id
+        collection_name = job.collection_name
+        
+        key = project_id + collection_name
 
         if not event_lists.has_key? project_id
           event_lists[project_id] = {
-            :auth_token => job.auth_token,
             :events => [],
           }
         end
 
         event_lists[project_id][:events] << job.event_body
-        event_lists[project_id][:events] << job.event_body
+        # TODO:  collate errrthing
 
       end
 
 
-
       event_list.each do |key, value|
+        
       end
 
     end
