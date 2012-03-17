9da5aab32721a718c9065a84e9e36b4d6011129f
dorkitude <kyle@kylewild.com>
2012-03-16 21:07:30 -0500

    Make the Rails example track every pageview with Keen

--- examples/rails_2/CoolForums/app/controllers/application_controller.rb
+++ examples/rails_2/CoolForums/app/controllers/application_controller.rb
@@ -4,6 +4,26 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  before_filter :track_pageview
+
+
+  # Runs before every request:
+  def track_pageview
+
+    # Get these from the keen.io website:
+    project_id = "4f5775ad163d666a6100000e"
+    auth_token = "a5d4eaf432914823a94ecd7e0cb547b9"
+
+    # First you must setup the client:
+    keen = Keen::Client.new(project_id, auth_token)
+
+    # Log the event with Keen:
+    keen.add_event("pageviews", {
+      :params   => params,
+      :url      => request.url,
+    })
+
+  end
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
